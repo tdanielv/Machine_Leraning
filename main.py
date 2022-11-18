646d370ed94b9913f3572a89573c8a6a7bea9976
Daniil Tkalich <103593586+tdanielv@users.noreply.github.com>
2022-11-18 19:30:02 +0400

    stepik task 2.5 last

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import numpy as np
-from scipy import sparse
-from sklearn import datasets
 
-matrix = np.array([[7, 8, 5, 5, 3], [10, 11, 6, 7, 5], [5, 3, 6, 2, 5], [6, 7, 5, 4, 2], [7, 10, 7, 5, 0]])
 
-print(np.linalg.det(matrix))
+B = np.array([[5, 6], [7, 8]])
+A = np.array([[3, -1], [5, -2]])
+C = np.array([[14, 16], [9, 10]])
+
+b = np.linalg.inv(B)
+a = np.linalg.inv(A)
+
+print(b)
+print(a)
+print(a @ C @ b )
+print(np.sum(a @ C @ b))
